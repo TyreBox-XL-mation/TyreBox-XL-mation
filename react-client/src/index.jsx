7305ea0b768a3e97bdde6ed8118a47b19b8a63b9
ines <piazzese.ines@gmail.com>
2021-02-16 15:58:42 +0100

    welcome

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,94 +1,76 @@
-import React from 'react';
-import Home from './components/Home.jsx'
-import Ourbrands from './components/Ourbrands.jsx'
-
-
+import React from "react";
+import Home from "./components/Home.jsx";
+import Ourbrands from "./components/Ourbrands.jsx";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            view: 'home',
-        }
-        this.changeView = this.changeView.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: "home",
+    };
+    this.changeView = this.changeView.bind(this);
+  }
+
+  //!show pages function
+  changeView(option) {
+    this.setState({
+      view: option,
+    });
+  }
+
+  //! The render view funstion
+  renderView() {
+    const { view } = this.state;
+    if (view === "home") {
+      return <Home />;
+    } else if (view === "ourbrands") {
+      return <Ourbrands />;
+    } else {
+      return <Contactus />;
     }
+  }
 
-    //!show pages function
-    changeView(option) {
+  render() {
+    return (
+      <div>
+        <nav>
+          <div className="nav">
+            <img
+              src=""
+              className="home"
+              onClick={() => this.changeView("home")}
+            />
 
-        this.setState({
-            view: option
-        });
-    }
+            <span
+              className="ourbrands"
+              onClick={() => this.changeView("ourbrands")}
+            >
+              <button type="button" id="ourbrands" className="btn btn-primary">
+                Our Brands
+              </button>
+            </span>
 
+            <span
+              className={
+                this.state.view === "home" ? "nav-selected" : "nav-unselected"
+              }
+            >
+              <button
+                type="button"
+                className="btn btn-success"
+                id="contactus"
+                onClick={() => this.changeView("contactus")}
+              >
+                Contact Us
+              </button>
+            </span>
+          </div>
+        </nav>
 
-    //! The render view funstion
-    renderView() {
-        const { view } = this.state;
-        if (view === "home") {
-            return <Home />;
-        }
-        else if (view === "ourbrands") {
-            return <Ourbrands />;
-        } else {
-            return <Contactus />
-        }
-    }
-
-
-    render() {
-
-        return (
-
-            <div>
-                <nav>
-
-                    <div className="nav">
-                        <img src='' className="home" onClick={() => this.changeView('home')} />
-
-
-
-
-                        <span className="ourbrands"
-                            onClick={() => this.changeView('ourbrands')}>
-                            <button type="button" id='ourbrands' className="btn btn-primary">Our Brands</button>
-                        </span>
-
-
-                        <span className={this.state.view === 'home'
-                            ? 'nav-selected'
-                            : 'nav-unselected'}
-                        >
-                            <button type="button" className="btn btn-success" id='contactus' onClick={() => this.changeView('contactus')} >Contact Us</button>
-
-                        </span>
-
-
-
-
-                    </div>
-                </nav>
-
-                <div className="main">
-
-                    {this.renderView()}
-                </div>
-            </div >
-
-        )
-    }
-
-
-
-
-
-
-
-
-
-
-
-
+        <div className="main">{this.renderView()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
