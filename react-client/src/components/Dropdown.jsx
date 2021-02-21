c8a3d067d635e5652c5ff3fa55e62b9d059926b8
jdidi94 <jdididaoud404@gmail.com>
2021-02-21 15:54:13 +0100

    jdidi

--- react-client/src/components/Dropdown.jsx
+++ react-client/src/components/Dropdown.jsx
@@ -1,79 +1,88 @@
+import React from "react";
 
-import React from 'react';
-
-
+// dropdown component
 class Dropdown extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            type: '',
-            width: '',
-            diameter: '',
-            height: '',
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: "",
+      width: "",
+      diameter: "",
+      height: "",
+    };
+  }
 
-    }
+  handelevent(event) {
+    console.log(event.target.name);
+    console.log(event.target.value);
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
 
-    handelevent(event) {
-        console.log(event.target.name)
-        console.log(event.target.value)
-        this.setState({
-            [event.target.name]: event.target.value
-        })
+  handleClick(e) {
+    e.preventDefault();
+  }
 
-    }
+  render() {
+    console.log(this.state);
+    return (
+      <div className="dropdownbtn">
+        <h1 id="seachtitle"> Search for your favorite tyre </h1>
+        <select
+          name="type"
+          onChange={this.handelevent.bind(this)}
+          className="form-select"
+          aria-label="Default select example"
+        >
+          <option defaultValue>Select Tyre Type (BRAND)</option>
+          <option>Amine</option>
+          <option>GoodYear</option>
+          <option>Bridgestone</option>
+        </select>
+        <select
+          name="width"
+          onChange={this.handelevent.bind(this)}
+          className="form-select"
+          aria-label="Default select example"
+        >
+          <option defaultValue>Select Width</option>
+          <option>195</option>
+          <option>200</option>
+          <option>205</option>
+          <option>175</option>
+        </select>
+        <select
+          name="diameter"
+          onChange={this.handelevent.bind(this)}
+          className="form-select"
+          aria-label="Default select example"
+        >
+          <option defaultValue>Select Diameter</option>
+          <option>55</option>
+          <option>60</option>
+          <option>58</option>
+          <option>75</option>
+        </select>
+        <select
+          name="height"
+          onChange={this.handelevent.bind(this)}
+          className="form-select"
+          aria-label="Default select example"
+        >
+          <option defaultValue>Select Height</option>
+          <option>15</option>
+          <option>16</option>
+          <option>18</option>
+          <option>19</option>
+        </select>
 
-    handleClick(e) {
-        e.preventDefault()
-
-    }
-
-
-    render() {
-        console.log(this.state)
-        return (
-
-            <div className='dropdownbtn'>
-                <h1 id='seachtitle'> Search for your favorite tyre  </h1>
-                <select name='type' onChange={this.handelevent.bind(this)} className="form-select" aria-label="Default select example">
-                    <option defaultValue>Select Tyre Type (BRAND)</option>
-                    <option >Amine</option>
-                    <option >GoodYear</option>
-                    <option >Bridgestone</option>
-                </select>
-                <select name='width' onChange={this.handelevent.bind(this)} className="form-select" aria-label="Default select example">
-                    <option defaultValue>Select Width</option>
-                    <option >195</option>
-                    <option >200</option>
-                    <option >205</option>
-                    <option >175</option>
-                </select>
-                <select name='diameter' onChange={this.handelevent.bind(this)} className="form-select" aria-label="Default select example">
-                    <option defaultValue>Select Diameter</option>
-                    <option >55</option>
-                    <option >60</option>
-                    <option >58</option>
-                    <option >75</option>
-                </select>
-                <select name='height' onChange={this.handelevent.bind(this)} className="form-select" aria-label="Default select example">
-                    <option defaultValue>Select Height</option>
-                    <option >15</option>
-                    <option >16</option>
-                    <option >18</option>
-                    <option >19</option>
-                </select>
-
-                <button id='buttonsearch' type="button" className="btn btn-dark">Search</button>
-            </div>
-
-        )
-    }
-
-
-
-
-
-
+        <button id="buttonsearch" type="button" className="btn btn-dark">
+          Search
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Dropdown;
